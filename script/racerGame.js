dca2155ace4f1c3163117a9df5ab1a8776397dda
Abhi Kala <abhi.kala123@gmail.com>
2016-04-24 19:23:14 +1200

    add basic game logic - start and end states

--- script/racerGame.js
+++ script/racerGame.js
@@ -6,15 +6,14 @@
 
   var player1_items = player1.querySelectorAll("td")
   var player2_items = player2.querySelectorAll("td")
-  console.log(player1_items)
+
 
 // find the code for the pressed key
   document.onkeyup = function(e){
     console.log(e.which);
   };
 
-// Start state of the game
-
+// Set start state of the game
 function startGame(playerItems){
   for(var i = 0; i < playerItems.length; i++ ){
     // console.log(playerItems[i])
@@ -24,5 +23,33 @@ function startGame(playerItems){
 };
 startGame(player1_items);
 startGame(player2_items);
+
+// update player position function
+function updatePlayerPosition(player){
+  for(var i = 0; i < player.length; i++ ){
+    if(player[i].className === "active"){
+      nextElement = player[i].nextElementSibling;
+      // console.log(nextElement);
+      if(player[i] === player[3]){
+        alert("Game Over")
+      }else{
+        nextElement.className = "active";
+        player[i].className = "";
+      };
+    break;
+    };
+  };
+};
+
+// run the update player funtion based on the key press
+document.onkeyup = function whichKey(e){
+  if (e.which === 81) {
+    updatePlayerPosition(player1_items)
+  }else if (e.which === 80){
+    updatePlayerPosition(player2_items)
+  }else{
+    alert("Press 'Q' to move player 1 and 'P' to move player 2")
+  };
+};
 
 // });
